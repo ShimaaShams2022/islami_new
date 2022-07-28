ca81aa144e74e2042a3516630b809f0fdb88e28d
salma <shimaa.shams@gmail.com>
2022-07-28 03:45:01 +0200

    initialize app first Git

---
+++ lib/home_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  static const String routeName = 'Home';
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
